bca5ba00845066e5b1b5230749bbcc93bcd01113
Caleb <cnkitzmann@gmail.com>
2019-10-30 13:04:08 -0400

    random check every 30mins

--- index.js
+++ index.js
@@ -3,24 +3,59 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
+function getRandom(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+async function doot() {
+	let channel = client.channels.forEach(async (channel) => {
+		if (channel.type == 'voice' && channel.members.size > 0) {
+			await channel.join().then(async (connection) => {
+				let dispatcher = await connection.playFile('./doot.mp3');
+				await dispatcher.on('end', function() {
+					channel.leave();
+				});
+			});
+		}
+	});
+}
+
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
+	// doot();
+	var handle = setInterval(function() {
+		var rand = getRandom(0, 10);
+		console.log(rand);
+		if (rand == 10) {
+			doot();
+		}
+	}, 1000 * 60 * 30);
 });
 
 client.on('message', async msg => {
-	if (msg.content === 'ping') {
-		if (msg.member.voiceChannel) {
-			var voiceChannel = msg.member.voiceChannel;
-			voiceChannel.join().then(connection => {
-				const dispatcher = connection.playFile('./doot.mp3');
-				dispatcher.on('end', end => {
-					voiceChannel.leave()
-				})
-			}).catch(err => console.log(err));
-		} else {
-			msg.reply('You need to be in a voice channel first!');
-		}
+	if (msg.content === 'doot') {
+		doot();
 	}
 });
+
+// client.on('voiceStateUpdate', (oldMember, newMember) => {
+// 	let newUserChannel = newMember.voiceChannel;
+// 	let oldUserChannel = oldMember.voiceChannel;
+
+// 	if (oldUserChannel === undefined && newUserChannel !== undefined) {
+// 		// User joined a voice channel
+// 		console.log('User joined channel');
+// 		var handle = setInterval(function() {
+// 			var rand = getRandom(0, 10);
+// 			console.log(rand);
+// 			if (rand == 10) {
+// 				doot();
+// 			}
+// 		}, 1000);
+// 	} else if (newUserChannel === undefined) {
+// 		// User left a voice channel
+// 		clearInterval(handle);
+// 	}
+// });
 
 client.login(process.env.BOT_TOKEN);
