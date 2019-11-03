c7a10b5b1878f59e6cfd1189d9582a0b8149a052
Caleb <cnkitzmann@gmail.com>
2019-11-03 18:05:04 -0500

    cooldown on johnnyboi

--- events/message.js
+++ events/message.js
@@ -1,6 +1,8 @@
 const doot = require('../commands/doot')
 const embed = require('../commands/embed')
 
+const talkedRecently = new Set()
+
 module.exports = (client, message) => {
     const command = String(message.content).toLowerCase()
 
@@ -12,7 +14,15 @@ module.exports = (client, message) => {
         return doot(message, './media/doot.mp3')
     }
     else if (command.includes('johnnyboi')) {
-        return doot(message, './media/dootcena.mp3')
+        if (talkedRecently.has(message.author.id)) {
+            message.reply('No')
+        } else {
+            talkedRecently.add(message.author.id)
+            setTimeout(() => {
+                talkedRecently.delete(message.author.id)
+            }, 1000 * 60 * 5)
+            return doot(message, './media/dootcena.mp3')
+        }
     }
     else if (command.includes('spoopy')) {
         embed(message, 'https://media1.tenor.com/images/a369655bae5b5f8e8de548e631e80d19/tenor.gif?itemid=14585469', 'Let\'s get spoopy ;)')
