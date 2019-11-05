8ccac0be6dde6f1878c20ced42fd5b3e4db06397
Caleb <cnkitzmann@gmail.com>
2019-11-05 02:19:07 -0500

    more refactoring and better command handling

--- commands/doot.js
+++ commands/doot.js
@@ -1,38 +1,89 @@
-var servers = {}
+const embed = require('../functions/embed')
 
-module.exports = (message, file, vol) => {
-    if (!servers[message.guild.id]) servers[message.guild.id] = {
-        queue: []
-    }
-    var server = servers[message.guild.id]
-    if (!message.guild.voiceConnection && message.member.voiceChannel) {
-        console.log(`Joining \"${message.guild.name}/${message.member.voiceChannel.name}\" & Playing \"${file}\"`)
-        server.queue.push({file: file, vol: (vol || 1)})
-        message.member.voiceChannel.join().then(function (connection) {
-            qPlay(connection, message)
-        })
-    }
-    else {
+module.exports = {
+    name: 'doot',
+    description: 'doot doot',
+    async execute(message) {
+        const args = message.content.split(' ')
+        const queue = message.client.queue
+        const serverQueue = message.client.queue.get(message.guild.id)
+
+        var voiceChannel = message.member.voiceChannel
+        if (!voiceChannel) {
+            voiceChannel = this.findChannel(message)
+            if (!voiceChannel) return
+        }
+        const permissions = voiceChannel.permissionsFor(message.client.user)
+        if (!permissions.has('CONNECT') || !permissions.has('SPEAK')) {
+            return message.channel.send('I need permission to join and speak in your channel!')
+        }
+
+        const command = args[1]
+
+        if (!serverQueue) {
+            const queueConstruct = {
+                textChannel: message.channel,
+                voiceChannel: voiceChannel,
+                commands: [],
+                connection: null,
+                playing: true,
+            }
+
+            queue.set(message.guild.id, queueConstruct)
+            
+            queueConstruct.commands.push(command)
+
+            try {
+                var connection = await voiceChannel.join()
+                queueConstruct.connection = connection
+                this.play(message, queueConstruct.commands[0])
+            } catch (err) {
+                console.log(err)
+                queue.delete(message.guild.id)
+                return message.channel.send(err)
+            }
+        } else {
+            serverQueue.commands.push(command)
+        }
+    },
+    play(message, command) {
+        const queue = message.client.queue
+        const guild = message.guild
+        const serverQueue = queue.get(message.guild.id)
+
+        const fileRef = [
+            { name: 'doot', path: './media/doot.mp3' },
+            { name: 'spoopy', path: './media/spooky-scary-skullingtumpets.mp3', vol: 0.2 },
+            { name: 'johnnyboi', path: './media/dootcena.mp3'}
+        ]
+        const file = fileRef.find(o => o.name === command)
+
+        if (!file) {
+            serverQueue.voiceChannel.leave()
+            queue.delete(guild.id)
+            return
+        }
+
+        if (command == 'spoopy') {
+            embed(message, 'https://media1.tenor.com/images/a369655bae5b5f8e8de548e631e80d19/tenor.gif?itemid=14585469', 'Let\'s get spoopy ;)')
+                .then(msg => { msg.delete(2200 * 60) })
+        }
+
+        const dispatcher = serverQueue.connection.playFile(file.path, {volume: file.vol})
+            .on('end', () => {
+                console.log('dooted!')
+                serverQueue.commands.shift()
+                this.play(message, serverQueue.commands[0])
+            })
+            .on('error', err => {
+                console.error(err)
+            })
+    },
+    findChannel(message) {
         message.guild.channels.forEach(channel => {
             if (channel.type == 'voice' && channel.members.size > 0 && !message.guild.voiceConnection) {
-                console.log(`Joining \"${message.guild.name}/${channel.name}\" & Playing \"${file}\"`)
-                server.queue.push({file: file, vol: (vol || 1)})
-                channel.join().then(function (connection) {
-                    qPlay(connection, message)
-                })
+                return channel
             }
         })
     }
-}
-
-function qPlay(connection, message) {
-    var server = servers[message.guild.id]
-
-    server.dispatcher = connection.playFile(server.queue[0].file, {volume: server.queue[0].vol})
-    server.queue.shift()
-
-    server.dispatcher.on('end', function() {
-        if (server.queue[0]) qPlay(connection, message)
-        else connection.disconnect()
-    })
 }
